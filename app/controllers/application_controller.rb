8ce92440ca9fbdb72a5ee962585194d5fed88f5e
Garrett <garres4@vt.edu>
2022-02-28 16:11:41 -0700

    guard clauses functional

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::API
 
     rescue_from ActiveRecord::RecordInvalid, with: :rescue_from_record_invalid
 
+    def rescue_from_record_invalid invalid
+        render json: { errors: invalid.record.errors.full_messages}, status: :bad_request
+    end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,21 +2,49 @@ class PostsController < ApplicationController
     require 'rest-client'
 
     def show
-        if params["tags"]
+        if (["id", "reads", "likes", "popularity"].include?(params['sortBy']) == false)
+
+            render json: {error: "sortBy parameter is invalid"}, status: :bad_request
+            
+        elsif(['asc', 'desc'].include?(params['direction']) == false)
+
+            render json: {error: "direction parameter is invalid"}, status: :bad_request
+
+        elsif params["tags"]
+
+            if params['sortBy'] == nil
+                params['sortBy'] = 'id'
+            end
+
+            if params['direction'] == nil
+                params['direction'] = 'asc'
+            end
+
         threads = []
         responses = []
+
         for index in params["tags"]
+            sortBy = params["sortBy"]
+            direction = params["direction"]
             threads << Thread.new(index) { |tag|
-                url = "https://api.hatchways.io/assessment/blog/posts?tag=#{tag}"
+                url = "https://api.hatchways.io/assessment/blog/posts?tag=#{tag}&sortBy=#{sortBy}&direction=#{direction}"
                 response = RestClient.get(url)
                 res_data = JSON.parse(response.body)
-                responses << res_data
+                responses << res_data["posts"]
             }
         end
-       threads.each { |thread| thread.join}
-       render json: responses, status: :ok
-    else
-        render json: {error: "Tags parameter is required"}, status: :bad_request
+            threads.each { |thread| thread.join}
+            flattened = responses.flatten
+            
+        if params['direction'] == 'desc'
+            sorted = flattened.sort_by{|obj| obj[params['sortBy']] }.reverse
+        else
+            sorted = flattened.sort_by{|obj| obj[params['sortBy']]}
+        end
+            render json: sorted, status: :ok
+        else
+            render json: {error: "Tags parameter is required"}, status: :bad_request
+        end
     end
 
 end
