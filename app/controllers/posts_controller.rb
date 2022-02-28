59ea1467752bd35cce048e8a33c440774a2efb15
Garrett <garres4@vt.edu>
2022-02-28 16:22:34 -0700

    fixed indents

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,8 @@ class PostsController < ApplicationController
         if (["id", "reads", "likes", "popularity"].include?(params['sortBy']) == false)
 
             render json: {error: "sortBy parameter is invalid"}, status: :bad_request
-            
-        elsif(['asc', 'desc'].include?(params['direction']) == false)
+
+        elsif (['asc', 'desc'].include?(params['direction']) == false)
 
             render json: {error: "direction parameter is invalid"}, status: :bad_request
 
@@ -20,27 +20,29 @@ class PostsController < ApplicationController
                 params['direction'] = 'asc'
             end
 
-        threads = []
-        responses = []
+            threads = []
+            responses = []
 
-        for index in params["tags"]
-            sortBy = params["sortBy"]
-            direction = params["direction"]
-            threads << Thread.new(index) { |tag|
-                url = "https://api.hatchways.io/assessment/blog/posts?tag=#{tag}&sortBy=#{sortBy}&direction=#{direction}"
-                response = RestClient.get(url)
-                res_data = JSON.parse(response.body)
-                responses << res_data["posts"]
-            }
-        end
+            for index in params["tags"]
+                sortBy = params["sortBy"]
+                direction = params["direction"]
+                threads << Thread.new(index) { |tag|
+                    url = "https://api.hatchways.io/assessment/blog/posts?tag=#{tag}&sortBy=#{sortBy}&direction=#{direction}"
+                    response = RestClient.get(url)
+                    res_data = JSON.parse(response.body)
+                    responses << res_data["posts"]
+                }
+            end
+
             threads.each { |thread| thread.join}
             flattened = responses.flatten
             
-        if params['direction'] == 'desc'
-            sorted = flattened.sort_by{|obj| obj[params['sortBy']] }.reverse
-        else
-            sorted = flattened.sort_by{|obj| obj[params['sortBy']]}
-        end
+            if params['direction'] == 'desc'
+                sorted = flattened.sort_by{|obj| obj[params['sortBy']] }.reverse
+            else
+                sorted = flattened.sort_by{|obj| obj[params['sortBy']]}
+            end
+            
             render json: sorted, status: :ok
         else
             render json: {error: "Tags parameter is required"}, status: :bad_request
