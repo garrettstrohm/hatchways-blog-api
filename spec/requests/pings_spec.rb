6de8e28345ac7aeb4f10b252ff72bc26b60a4418
Garrett <garres4@vt.edu>
2022-03-01 09:52:39 -0700

    testing done

--- spec/requests/pings_spec.rb
+++ spec/requests/pings_spec.rb
@@ -4,8 +4,11 @@ RSpec.describe "Pings", type: :request do
   describe "GET /api/ping" do
     it "sends status code of :ok" do
       get '/api/ping'
+      parsed_body = JSON.parse(response.body)
       expect(response).to have_http_status(:ok)
-      expect(response.body).to eq("{\"success\":true}")
+      expect(parsed_body).to include_json({
+        success: true
+      })
     end
   end
 end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -10,6 +10,9 @@ RSpec.describe "Posts", type: :request do
     it "returns an array of objects" do
       get "/api/posts", params: {tags: ["science", "health"], sortBy: "likes", direction: "asc"}
       expect(response).to have_http_status(:ok)
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body).to be_an_instance_of(Array)
+      expect(parsed_body[0]).to be_an_instance_of(Hash)
     end
 
     it "throws error message with incorrect params" do
