a28299c20f449b25123a6c7714c0a3cbdfed21e5
Garrett <garres4@vt.edu>
2022-03-01 09:42:40 -0700

    got some tests done

--- spec/requests/pings_spec.rb
+++ spec/requests/pings_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Pings", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /api/ping" do
+    it "sends status code of :ok" do
+      get '/api/ping'
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to eq("{\"success\":true}")
+    end
   end
 end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /api/posts" do
+    it "throws error message without params" do
+      get "/api/posts"
+      expect(response).to have_http_status(:bad_request)
+    end
+
+    it "returns an array of objects" do
+      get "/api/posts", params: {tags: ["science", "health"], sortBy: "likes", direction: "asc"}
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "throws error message with incorrect params" do
+      get "/api/posts", params: {tags: ['science'], sortBy: "cheese", direction: "desc"}
+      expect(response).to have_http_status(:bad_request)
+    end
+
   end
+
 end
